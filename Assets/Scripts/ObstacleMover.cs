efb0d847d5efd0dd77d8966b1d77a267736b1d97
kumartul <kumartul001@gmail.com>
2022-05-30 12:30:52 +0530

    [Cache]: The 'Transform' component of Obstacle

--- Assets/Scripts/ObstacleMover.cs
+++ Assets/Scripts/ObstacleMover.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class ObstacleMover : MonoBehaviour
 {
+    private Transform _myTransform;     // Cache the Transform component of this object
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _myTransform = transform;
+    }
+
     // Update is called once per frame
     private void Update()
     {
         // Move the obstacle to the left
-        transform.position += Vector3.left * Time.deltaTime * PlayerController.Speed;
+        _myTransform.position += Vector3.left * Time.deltaTime * PlayerController.Speed;
     }
 }
